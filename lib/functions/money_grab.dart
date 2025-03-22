dbed504d4bbf46e15c5eea3358d6291730f23621
Odinn breki <odinnbreki@outlook.com>
2025-03-22 11:29:15 +0000

    money grab

--- lib/functions/money_grab.dart
+++ lib/functions/money_grab.dart
@@ -19,9 +19,39 @@ he doesn't input a integer?
  */
 
 int PlaceBet(int bankRoll){
+  print("Place a bet!");
+  String bet = stdin.readLineSync()?? "";
+  int parsedBet = 0;
 
+  try {
+    parsedBet = int.parse(bet);
+    print("You bet $parsedBet");
+  }catch(e){
+    print("Please type a number for the bet you would like to place!");
+  }
+  return parsedBet;
 }
 
 int BuyIn(){
+  int buyIn = 10;
+  int bet = 0;
+  bool Betting = true;
+  while(Betting) {
 
+    String BuyIn = stdin.readLineSync() ?? '';
+    try {
+      bet = int.parse(BuyIn);
+    } catch (e) {
+      print('Please enter buy in number!');
+    }
+    if (bet <= buyIn) {
+      print("Cannot place a bet lower than buy in");
+      print("Please place a valid bet!");
+      continue;
+    } else if(bet >= buyIn){
+      print("You placed $bet for buy in");
+      break;
+    }
+  }
+  return bet;
 }
